77dab77e154682e98c750b26e05151ee60ef5ef2
Alex Delaveau <alex26delaveau@gmail.com>
2023-03-10 16:02:19 +0100

    Background home

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-
+    <div class="main">
+        <h2>RETOUR SOIREE</h2>
     </div>
 </template>
 
@@ -9,5 +9,10 @@
 </script>
 
 <style>
-
+.main{
+    color: #fff;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+}
 </style>
