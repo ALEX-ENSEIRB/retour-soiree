4d3a07149d1f9054708ca578ec1029c83f6157d5
Alex Delaveau <alex26delaveau@gmail.com>
2023-04-05 15:25:30 +0200

    Trip creation work

--- API/post_trip.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include "header.php";
-include "connect.php"; /* Le fichier connect.php contient les identifiants de connexion */
-
-
-//insert into lieux 
-var_dump($_POST);
-$requete = "INSERT INTO lieux (ADRESSE_LIEU, ID_ZONE) VALUES ()";
--- src/api/create-trip.js
+++ src/api/create-trip.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
 
-const API_URL = await fetch('/src/config.json').then((response) => response.json()).then((json) => json.API_URL);
+const API_URL = await fetch('/conf.json').then(async (response) => {
+    const responseJson = await response.json().then((json) => {
+        return json.api_url
+    })
+    return responseJson;
+});
 
-const  postTrip = (trip) => {
-    return axios.post(API_URL + "post_trip.php", trip).then((response) => {
+const createTrip = async (trip) => {
+    const data = await axios.post(API_URL + "create_trip.php", trip).then((response) => {
+        console.log(response.data);
         return response.data;
-    });
+    })
+    return data;
 }
 
-export default postTrip;
+export default createTrip;
