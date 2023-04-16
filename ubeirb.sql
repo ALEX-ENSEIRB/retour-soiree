032dd716244a2b09b2b6cf58cb3ae252cad113ad
Alex Delaveau <alex26delaveau@gmail.com>
2023-04-16 21:21:56 +0200

    maj sql script

--- ubeirb.sql
+++ ubeirb.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1
--- Généré le : lun. 27 mars 2023 à 11:13
--- Version du serveur : 10.4.27-MariaDB
--- Version de PHP : 8.2.0
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -18,52 +9,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `ubeirb`
+-- Base de données : `retour-soiree`
 --
-
--- --------------------------------------------------------
-
---
--- Structure de la table `commentaires_personnes`
---
-
-CREATE TABLE `commentaires_personnes` (
-  `ID_COMMENTAIRE` int(6) NOT NULL,
-  `NOTE` int(2) DEFAULT NULL,
-  `CONTENU_COMMENTAIRE` varchar(255) DEFAULT NULL,
-  `HEURE_COMMENTAIRE` time DEFAULT NULL,
-  `NO_ETUDIANT_CIBLE` int(6) DEFAULT NULL,
-  `NO_ETUDIANT_SOURCE` int(6) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `commentaires_trajets`
---
-
-CREATE TABLE `commentaires_trajets` (
-  `ID_COMMENTAIRE` int(6) NOT NULL,
-  `NOTE` int(2) DEFAULT NULL,
-  `CONTENU_COMMENTAIRE` varchar(255) DEFAULT NULL,
-  `HEURE_COMMENTAIRE` time DEFAULT NULL,
-  `NO_ETUDIANT` int(6) DEFAULT NULL,
-  `ID_TRAJET` int(6) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
---
-
-CREATE TABLE `users` (
-  `NO_ETUDIANT` int(6) NOT NULL,
-  `NOM_ETUDIANT` varchar(255) DEFAULT NULL,
-  `PRENOM_ETUDIANT` varchar(255) DEFAULT NULL,
-  `NOTEL_ETUDIANT` int(10) DEFAULT NULL,
-  `MAIL_ETUDIANT` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -73,8 +20,16 @@ CREATE TABLE `users` (
 
 CREATE TABLE `evenements` (
   `ID_EVENT` int(6) NOT NULL,
-  `TYPE_EVENT` varchar(255) DEFAULT NULL
+  `TYPE_EVENT` varchar(255) DEFAULT NULL,
+  `NOM_EVENT` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `evenements`
+--
+
+INSERT INTO `evenements` (`ID_EVENT`, `TYPE_EVENT`, `NOM_EVENT`) VALUES
+(1, 'Soiree', 'D2A');
 
 -- --------------------------------------------------------
 
@@ -88,19 +43,15 @@ CREATE TABLE `lieux` (
   `ID_ZONE` int(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Structure de la table `messages`
+-- Déchargement des données de la table `lieux`
 --
 
-CREATE TABLE `messages` (
-  `ID_MESSAGE` int(6) NOT NULL,
-  `CONTENU_MESSAGE` varchar(255) DEFAULT NULL,
-  `HEURE_MESSAGE` time DEFAULT NULL,
-  `NO_ETUDIANT` int(6) DEFAULT NULL,
-  `ID_TRAJET` int(6) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `lieux` (`ID_LIEU`, `ADRESSE_LIEU`, `ID_ZONE`) VALUES
+(17, '17B rue aurelie ducros 33140 Villenave D\'ornon', 2),
+(18, '4 rue gabriel fauré 33400 Talence', 1),
+(19, '17b rue aurelie ducros 33140', 1),
+(20, '17b rue aurelie ducros 33140', 2);
 
 -- --------------------------------------------------------
 
@@ -113,6 +64,14 @@ CREATE TABLE `participations` (
   `ID_TRAJET` int(6) NOT NULL,
   `GESTION` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `participations`
+--
+
+INSERT INTO `participations` (`NO_ETUDIANT`, `ID_TRAJET`, `GESTION`) VALUES
+(11, 8, 'true'),
+(19, 7, 'true');
 
 -- --------------------------------------------------------
 
@@ -133,13 +92,44 @@ CREATE TABLE `rattachement` (
 
 CREATE TABLE `trajets` (
   `ID_TRAJET` int(6) NOT NULL,
-  `DATE_TRAJET` date DEFAULT NULL,
-  `HEURE_TRAJET` time DEFAULT NULL,
+  `DATE_TRAJET` datetime NOT NULL DEFAULT current_timestamp(),
   `NB_PLACES` int(6) DEFAULT NULL,
   `ID_DEPART` int(6) DEFAULT NULL,
   `ID_ARRIVEE` int(6) DEFAULT NULL,
   `ID_EVENT` int(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `trajets`
+--
+
+INSERT INTO `trajets` (`ID_TRAJET`, `DATE_TRAJET`, `NB_PLACES`, `ID_DEPART`, `ID_ARRIVEE`, `ID_EVENT`) VALUES
+(7, '2023-04-15 15:43:11', 1, 17, 18, NULL),
+(8, '2023-04-15 22:50:00', 3, 19, 20, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `NO_ETUDIANT` int(6) NOT NULL,
+  `NOM_ETUDIANT` varchar(255) NOT NULL,
+  `PRENOM_ETUDIANT` varchar(255) NOT NULL,
+  `NOTEL_ETUDIANT` int(10) DEFAULT NULL,
+  `MAIL_ETUDIANT` varchar(255) NOT NULL,
+  `MOT_DE_PASSE` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`NO_ETUDIANT`, `NOM_ETUDIANT`, `PRENOM_ETUDIANT`, `NOTEL_ETUDIANT`, `MAIL_ETUDIANT`, `MOT_DE_PASSE`) VALUES
+(11, 'Delaveau', 'Alex', 659439048, 'alex@alex.com', '$2y$10$0Kr3z/n69hpbXZQhLqnEjOM5VWfkaKo4WOEINXhwzbIv9mUkcIBCK'),
+(19, 'Delaveau', 'Alex', 659439048, 'alex2@alex.com', '$2y$10$oVoH3NcMyUXkxQ4wtiuTC.sOCSC0sXda6QVW8RtZ804DkJMTvGRgm'),
+(29, '', '', 0, '', '$2y$10$.elzqQN3nDywGCQkdJlm6ux9bTBafYKwECUxO9wRFGTP8f6fjjqyu');
 
 -- --------------------------------------------------------
 
@@ -153,30 +143,23 @@ CREATE TABLE `zones` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Déchargement des données de la table `zones`
+--
+
+INSERT INTO `zones` (`ID_ZONE`, `NOM_ZONE`) VALUES
+(1, 'VICTOIRE'),
+(2, 'ENSEIRB'),
+(3, 'PESSAC'),
+(4, 'RIVE-DROITE'),
+(5, 'BEGLES'),
+(6, 'MERIGNAC'),
+(7, 'VILLENAVE-DORNON'),
+(8, 'GRADIGNAN'),
+(9, 'AUTRES');
+
+--
 -- Index pour les tables déchargées
 --
-
---
--- Index pour la table `commentaires_personnes`
---
-ALTER TABLE `commentaires_personnes`
-  ADD PRIMARY KEY (`ID_COMMENTAIRE`),
-  ADD KEY `NO_ETUDIANT_CIBLE` (`NO_ETUDIANT_CIBLE`),
-  ADD KEY `NO_ETUDIANT_SOURCE` (`NO_ETUDIANT_SOURCE`);
-
---
--- Index pour la table `commentaires_trajets`
---
-ALTER TABLE `commentaires_trajets`
-  ADD PRIMARY KEY (`ID_COMMENTAIRE`),
-  ADD KEY `NO_ETUDIANT` (`NO_ETUDIANT`),
-  ADD KEY `ID_TRAJET` (`ID_TRAJET`);
-
---
--- Index pour la table `etudiants`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`NO_ETUDIANT`);
 
 --
 -- Index pour la table `evenements`
@@ -190,14 +173,6 @@ ALTER TABLE `evenements`
 ALTER TABLE `lieux`
   ADD PRIMARY KEY (`ID_LIEU`),
   ADD KEY `ID_ZONE` (`ID_ZONE`);
-
---
--- Index pour la table `messages`
---
-ALTER TABLE `messages`
-  ADD PRIMARY KEY (`ID_MESSAGE`),
-  ADD KEY `NO_ETUDIANT` (`NO_ETUDIANT`),
-  ADD KEY `ID_TRAJET` (`ID_TRAJET`);
 
 --
 -- Index pour la table `participations`
@@ -223,6 +198,13 @@ ALTER TABLE `trajets`
   ADD KEY `ID_EVENT` (`ID_EVENT`);
 
 --
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`NO_ETUDIANT`),
+  ADD UNIQUE KEY `MAIL_ETUDIANT` (`MAIL_ETUDIANT`);
+
+--
 -- Index pour la table `zones`
 --
 ALTER TABLE `zones`
@@ -233,83 +215,44 @@ ALTER TABLE `zones`
 --
 
 --
--- AUTO_INCREMENT pour la table `commentaires_personnes`
---
-ALTER TABLE `commentaires_personnes`
-  MODIFY `ID_COMMENTAIRE` int(6) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `commentaires_trajets`
---
-ALTER TABLE `commentaires_trajets`
-  MODIFY `ID_COMMENTAIRE` int(6) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `etudiants`
---
-ALTER TABLE `users`
-  MODIFY `NO_ETUDIANT` int(6) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT pour la table `evenements`
 --
 ALTER TABLE `evenements`
-  MODIFY `ID_EVENT` int(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_EVENT` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `lieux`
 --
 ALTER TABLE `lieux`
-  MODIFY `ID_LIEU` int(6) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `messages`
---
-ALTER TABLE `messages`
-  MODIFY `ID_MESSAGE` int(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_LIEU` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT pour la table `trajets`
 --
 ALTER TABLE `trajets`
-  MODIFY `ID_TRAJET` int(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_TRAJET` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `NO_ETUDIANT` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT pour la table `zones`
 --
 ALTER TABLE `zones`
-  MODIFY `ID_ZONE` int(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_ZONE` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Contraintes pour les tables déchargées
 --
 
 --
--- Contraintes pour la table `commentaires_personnes`
---
-ALTER TABLE `commentaires_personnes`
-  ADD CONSTRAINT `commentaires_personnes_ibfk_1` FOREIGN KEY (`NO_ETUDIANT_CIBLE`) REFERENCES `users` (`NO_ETUDIANT`),
-  ADD CONSTRAINT `commentaires_personnes_ibfk_2` FOREIGN KEY (`NO_ETUDIANT_SOURCE`) REFERENCES `users` (`NO_ETUDIANT`);
-
---
--- Contraintes pour la table `commentaires_trajets`
---
-ALTER TABLE `commentaires_trajets`
-  ADD CONSTRAINT `commentaires_trajets_ibfk_1` FOREIGN KEY (`NO_ETUDIANT`) REFERENCES `users` (`NO_ETUDIANT`),
-  ADD CONSTRAINT `commentaires_trajets_ibfk_2` FOREIGN KEY (`ID_TRAJET`) REFERENCES `trajets` (`ID_TRAJET`);
-
---
 -- Contraintes pour la table `lieux`
 --
 ALTER TABLE `lieux`
   ADD CONSTRAINT `lieux_ibfk_1` FOREIGN KEY (`ID_ZONE`) REFERENCES `zones` (`ID_ZONE`);
-
---
--- Contraintes pour la table `messages`
---
-ALTER TABLE `messages`
-  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`NO_ETUDIANT`) REFERENCES `users` (`NO_ETUDIANT`),
-  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`ID_TRAJET`) REFERENCES `trajets` (`ID_TRAJET`);
 
 --
 -- Contraintes pour la table `participations`
